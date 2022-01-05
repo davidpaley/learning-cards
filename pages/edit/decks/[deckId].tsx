94236591b1f3af641f9601017b52d5ea8e878517
davidpaley <dani.montanaro21@gmail.com>
2022-01-05 16:17:17 -0300

    adding edit card in deck

--- pages/edit/decks/[deckId].tsx
+++ pages/edit/decks/[deckId].tsx
@@ -1,34 +1,66 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-
-import { Layout } from "antd";
 import styles from "../../../styles/EditDecks.module.css";
+
+import { Layout, Menu, Breadcrumb, Card, Button, Tooltip } from "antd";
+import { DeleteOutlined, PlusOutlined } from "@ant-design/icons";
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 const Home: NextPage = () => {
   return (
-    <Layout>
-      <Head>
-        <title>Edit Deck</title>
-        <meta name="description" content="Learn with space repetition!" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Edit Deck</h1>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider collapsible>
+        <div className={styles.addcard}>
+          <Button shape="circle" icon={<PlusOutlined />} />
+        </div>
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={["1"]}
+          mode="inline"
+          style={{ paddingTop: 40 }}
         >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+          <Menu.Item key="1" style={{ padding: 5, minHeight: "150px" }}>
+            <Card
+              title="Card title 1"
+              bordered={false}
+              className={styles.cards}
+            >
+              <Button type="primary" shape="circle" icon={<DeleteOutlined />} />
+            </Card>
+          </Menu.Item>
+          <Menu.Item key="2" style={{ padding: 5, minHeight: "150px" }}>
+            <Card
+              title="Card title 2"
+              bordered={false}
+              className={styles.cards}
+            >
+              <Button type="primary" shape="circle" icon={<DeleteOutlined />} />
+            </Card>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header />
+        <Content>
+          <Breadcrumb className={styles.breadcrumb}>
+            <Breadcrumb.Item>Card1</Breadcrumb.Item>
+          </Breadcrumb>
+          <div>
+            <div className={styles.card}>
+              <div className={styles.title}>Card Title</div>
+              <div className={styles.description}>
+                <p>Card content</p>
+              </div>
+            </div>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
     </Layout>
   );
 };
