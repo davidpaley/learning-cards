4b89e4fe33199d352b5250eb8c92fa96b140444d
davidpaley <dani.montanaro21@gmail.com>
2022-01-06 17:56:02 -0300

    improving styles

--- pages/edit/decks/[deckId].tsx
+++ pages/edit/decks/[deckId].tsx
@@ -1,66 +1,95 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../../../styles/EditDecks.module.css";
 
-import { Layout, Menu, Breadcrumb, Card, Button, Tooltip } from "antd";
+import { Layout, Menu, Breadcrumb, Card, Button, Input, Row } from "antd";
+
 import { DeleteOutlined, PlusOutlined } from "@ant-design/icons";
 
 const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
+const { TextArea } = Input;
 
 const Home: NextPage = () => {
+  const onChange = (e) => {
+    console.log("Change:", e.target.value);
+  };
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Sider collapsible>
-        <div className={styles.addcard}>
-          <Button shape="circle" icon={<PlusOutlined />} />
-        </div>
-        <Menu
-          theme="dark"
-          defaultSelectedKeys={["1"]}
-          mode="inline"
-          style={{ paddingTop: 40 }}
-        >
-          <Menu.Item key="1" style={{ padding: 5, minHeight: "150px" }}>
-            <Card
-              title="Card title 1"
-              bordered={false}
-              className={styles.cards}
+    <Layout>
+      <Header />
+      <Layout>
+        <Sider>
+          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+            <Menu.Item key="1" style={{ padding: 5, minHeight: "150px" }}>
+              {/* <Button
+                  type="ghost"
+                  shape="circle"
+                  className={styles.deleteCardButton}
+                  icon={<DeleteOutlined />}
+                /> */}
+              <Card
+                title="Card title 1"
+                bordered={false}
+                className={styles.cards}
+              />
+            </Menu.Item>
+            <Menu.Item key="2" style={{ padding: 5, minHeight: "150px" }}>
+              <Card
+                title="Card title 2"
+                bordered={false}
+                className={styles.cards}
+              />
+            </Menu.Item>
+          </Menu>
+          <div className={styles.addCard}>
+            <Button
+              type="text"
+              className={styles.createNewCardButton}
+              icon={<PlusOutlined />}
             >
-              <Button type="primary" shape="circle" icon={<DeleteOutlined />} />
-            </Card>
-          </Menu.Item>
-          <Menu.Item key="2" style={{ padding: 5, minHeight: "150px" }}>
-            <Card
-              title="Card title 2"
-              bordered={false}
-              className={styles.cards}
-            >
-              <Button type="primary" shape="circle" icon={<DeleteOutlined />} />
-            </Card>
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout">
-        <Header />
+              Create New Card
+            </Button>
+          </div>
+        </Sider>
         <Content>
-          <Breadcrumb className={styles.breadcrumb}>
-            <Breadcrumb.Item>Card1</Breadcrumb.Item>
-          </Breadcrumb>
+          <Row align="middle" justify="space-between">
+            <Breadcrumb className={styles.breadcrumb}>
+              <Breadcrumb.Item>Name of the Class</Breadcrumb.Item>
+              <Breadcrumb.Item>Name of the Deck</Breadcrumb.Item>
+              <Breadcrumb.Item>Card1</Breadcrumb.Item>
+            </Breadcrumb>
+            <Button
+              danger
+              className={styles.deleteCardButton}
+              icon={<DeleteOutlined />}
+            >
+              Delete Card
+            </Button>
+          </Row>
           <div>
             <div className={styles.card}>
-              <div className={styles.title}>Card Title</div>
-              <div className={styles.description}>
-                <p>Card content</p>
+              <div className={styles.textarea}>
+                <TextArea
+                  maxLength={1000}
+                  style={{ padding: 10 }}
+                  className={styles.questionText}
+                  onChange={onChange}
+                  rows={10}
+                />
+              </div>
+              <div className={styles.textarea}>
+                <TextArea
+                  maxLength={1000}
+                  className={styles.answerText}
+                  onChange={onChange}
+                  rows={20}
+                />
               </div>
             </div>
           </div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Ant Design ©2018 Created by Ant UED
-        </Footer>
       </Layout>
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
     </Layout>
   );
 };
