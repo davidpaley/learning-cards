820684ce46eb834bca7bbe0076874218267330c2
davidpaley <david.paleyy@gmail.com>
2022-03-07 11:51:53 -0300

    feat: fix play card page, getting the correct cards, and showing them in order

--- pages/play/[deckId].tsx
+++ pages/play/[deckId].tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import type { NextPage } from "next";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Card as CardType } from "@prisma/client";
 import styles from "../../styles/playCards.module.css";
-import { useQuery, useMutation, useQueryClient } from "react-query";
-import { getDate, viewCardsForDates } from "../../utils/dates";
+// TODO: delete this functions if they are not necessary
+// import { getDate, viewCardsForDates } from "../../utils/dates";
 
 import { Layout, Button, Row, Card, Typography, Col } from "antd";
 const { Title } = Typography;
@@ -11,7 +11,6 @@ const { Header } = Layout;
 const prisma = new PrismaClient();
 
 export async function getServerSideProps(context) {
-  const cardsToShow = [];
   const { query } = context;
   const deck = await prisma.deck.findUnique({
     where: {
@@ -24,12 +23,11 @@ export async function getServerSideProps(context) {
       id: true,
     },
   });
-  const showCards = deck.cards.map((item) => {
-    const dateCard = getDate(item.nextReviewDate);
-    const viewCards = viewCardsForDates(dateCard, getDate(new Date()));
-    if (viewCards) {
-      cardsToShow.push(item);
-    }
+  const currentDate = new Date().setHours(0, 0, 0, 0);
+  const cardsToShow = deck.cards.filter((card) => {
+    const cardDate = new Date(card.nextReviewDate).setHours(0, 0, 0, 0);
+    console.log({ cardDate });
+    return currentDate >= cardDate;
   });
   return {
     props: {
@@ -38,113 +36,149 @@ export async function getServerSideProps(context) {
   };
 }
 
-const Home: NextPage<{ cardsToPlay: any }> = ({ cardsToPlay }) => {
-  const [answerButton, setAnswerButton] = useState(true);
+const PlayPage: NextPage<{ cardsToPlay: CardType[] }> = ({ cardsToPlay }) => {
+  const [revealAnswerButton, setRevealAnswerButton] = useState(true);
+  const [emptyCardsForToday, setEmptyCardsForToday] = useState(
+    !cardsToPlay.length
+  );
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const goToNextCard = () => {
+    setCurrentCardIndex((prevState) => prevState + 1);
+    setRevealAnswerButton(true);
+  };
   const nextCardAndSetLevel = (level) => {
-    setAnswerButton(true);
+    // TODO: modify current card according to answer
+
+    const isLastCard = cardsToPlay.length === currentCardIndex + 1;
+    if (isLastCard) {
+      setEmptyCardsForToday(true);
+      return;
+    }
+    goToNextCard();
   };
   return (
     <Layout>
       <Header />
       <div className={styles.siteCardBorderLessWrapper}>
-        <Row align="middle" justify="center">
-          <Card
-            title={answerButton ? "Answer" : "Question"}
-            //extra={<a href="#">More</a>}
-            style={{ width: 900, height: 600 }}
-          >
-            <p className={styles.questionOrAnswer}>
-              {!answerButton ? cardsToPlay[0].answer : cardsToPlay[0].question}
-            </p>
-          </Card>
-        </Row>
-        ;
-        <Row align="middle" justify="center">
-          {answerButton ? (
-            <Button
-              className={styles.revealAnswerButton}
-              type="primary"
-              shape="round"
-              onClick={() => setAnswerButton(false)}
+        {!emptyCardsForToday ? (
+          <>
+            <Row align="middle" justify="center">
+              <Card
+                title={revealAnswerButton ? "Answer" : "Question"}
+                //extra={<a href="#">More</a>}
+                style={{ width: 900, height: 600 }}
+              >
+                <p className={styles.questionOrAnswer}>
+                  {!revealAnswerButton
+                    ? cardsToPlay[currentCardIndex].answer
+                    : cardsToPlay[currentCardIndex].question}
+                </p>
+              </Card>
+            </Row>
+
+            <Row align="middle" justify="center">
+              {revealAnswerButton ? (
+                <Button
+                  className={styles.revealAnswerButton}
+                  type="primary"
+                  shape="round"
+                  onClick={() => setRevealAnswerButton(false)}
+                >
+                  <Title className={styles.answerTitle} level={5}>
+                    Reveal Answer
+                  </Title>
+                </Button>
+              ) : (
+                <div className={styles.allButtonLevel}>
+                  <Col span={4}>
+                    <Button
+                      type="primary"
+                      className={styles.buttonLevel1}
+                      onClick={() => {
+                        nextCardAndSetLevel("1");
+                      }}
+                    >
+                      <Title className={styles.answerTitle} level={5}>
+                        1
+                      </Title>
+                    </Button>
+                  </Col>
+                  <Col span={4}>
+                    <Button
+                      type="primary"
+                      className={styles.buttonLevel2}
+                      onClick={() => {
+                        nextCardAndSetLevel("2");
+                      }}
+                    >
+                      <Title className={styles.answerTitle} level={5}>
+                        2
+                      </Title>
+                    </Button>
+                  </Col>
+                  <Col span={4}>
+                    <Button
+                      type="primary"
+                      className={styles.buttonLevel3}
+                      onClick={() => {
+                        nextCardAndSetLevel("3");
+                      }}
+                    >
+                      <Title className={styles.answerTitle} level={5}>
+                        3
+                      </Title>
+                    </Button>
+                  </Col>
+                  <Col span={4}>
+                    <Button
+                      type="primary"
+                      className={styles.buttonLevel4}
+                      onClick={() => {
+                        nextCardAndSetLevel("4");
+                      }}
+                    >
+                      <Title className={styles.answerTitle} level={5}>
+                        4
+                      </Title>
+                    </Button>
+                  </Col>
+                  <Col span={4}>
+                    <Button
+                      type="primary"
+                      className={styles.buttonLevel5}
+                      onClick={() => {
+                        nextCardAndSetLevel("5");
+                      }}
+                    >
+                      <Title className={styles.answerTitle} level={5}>
+                        5
+                      </Title>
+                    </Button>
+                  </Col>
+                </div>
+              )}
+            </Row>
+          </>
+        ) : (
+          <Row align="middle" justify="center">
+            <Card
+              title={"No cards for today"}
+              //extra={<a href="#">More</a>}
+              // TODO: Review styles of this card
+              // style={{ width: 900, height: 600 }}
             >
-              <Title className={styles.answerTitle} level={5}>
-                Reveal Answer
-              </Title>
-            </Button>
-          ) : (
-            <div className={styles.allButtonLevel}>
-              <Col span={4}>
-                <Button
-                  type="primary"
-                  className={styles.buttonLevel1}
-                  onClick={() => {
-                    nextCardAndSetLevel("1");
-                  }}
-                >
-                  <Title className={styles.answerTitle} level={5}>
-                    1
-                  </Title>
-                </Button>
-              </Col>
-              <Col span={4}>
-                <Button
-                  type="primary"
-                  className={styles.buttonLevel2}
-                  onClick={() => {
-                    nextCardAndSetLevel("2");
-                  }}
-                >
-                  <Title className={styles.answerTitle} level={5}>
-                    2
-                  </Title>
-                </Button>
-              </Col>
-              <Col span={4}>
-                <Button
-                  type="primary"
-                  className={styles.buttonLevel3}
-                  onClick={() => {
-                    nextCardAndSetLevel("3");
-                  }}
-                >
-                  <Title className={styles.answerTitle} level={5}>
-                    3
-                  </Title>
-                </Button>
-              </Col>
-              <Col span={4}>
-                <Button
-                  type="primary"
-                  className={styles.buttonLevel4}
-                  onClick={() => {
-                    nextCardAndSetLevel("4");
-                  }}
-                >
-                  <Title className={styles.answerTitle} level={5}>
-                    4
-                  </Title>
-                </Button>
-              </Col>
-              <Col span={4}>
-                <Button
-                  type="primary"
-                  className={styles.buttonLevel5}
-                  onClick={() => {
-                    nextCardAndSetLevel("5");
-                  }}
-                >
-                  <Title className={styles.answerTitle} level={5}>
-                    5
-                  </Title>
-                </Button>
-              </Col>
-            </div>
-          )}
-        </Row>
+              <p className={styles.questionOrAnswer}>
+                {
+                  "You don't have any cards to resolve today! Go enjoy the day =)"
+                }
+              </p>
+            </Card>
+          </Row>
+        )}
       </div>
       );
     </Layout>
   );
 };
 
-export default Home;
+export default PlayPage;
