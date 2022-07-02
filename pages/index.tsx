a2f779ee38d604cadbf8a5d620d129169c13ea8f
davidpaley <david.paleyy@gmail.com>
2022-07-02 08:59:23 -0300

    fix: styles of root page, and package.json error

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { Button, Layout, Menu } from "antd";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Root.module.css";
 import Link from "next/link";
 
 const { Content, Footer, Header } = Layout;
@@ -15,8 +15,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Learn with space repetition!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header className="header">
-        <div className="logo" />
+      <Header>
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
           <Menu.Item key="1">Learn!</Menu.Item>
           <Menu.Item key="2">How it works?</Menu.Item>
@@ -32,32 +31,6 @@ const Home: NextPage = () => {
           <div className={styles.grid}>
             <h2>Start creating cards and learn more</h2>
             <br />
-            {/* {decks && decks.map((deck: any) => (
-              <p>{deck.name}</p>
-            ))} */}
-
-            {/* <a href="https://nextjs.org/learn" className={styles.card}>
-              <h2>Learn &rarr;</h2>
-              <p>Learn about Next.js in an interactive course with quizzes!</p>
-            </a>
-
-            <a
-              href="https://github.com/vercel/next.js/tree/master/examples"
-              className={styles.card}
-            >
-              <h2>Examples &rarr;</h2>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-
-            <a
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className={styles.card}
-            >
-              <h2>Deploy &rarr;</h2>
-              <p>
-                Instantly deploy your Next.js site to a public URL with Vercel.
-              </p>
-            </a> */}
           </div>
           <div>
             <Link href="/home">
