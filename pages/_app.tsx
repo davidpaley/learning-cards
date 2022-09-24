17dcf49346dbd6ecb4d1d5c563d96b7636f571cf
davidpaley <david.paleyy@gmail.com>
2022-09-24 17:34:56 -0300

    feat: first loading when changing the page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,22 @@ import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import React from "react";
 import { ReactQueryDevtools } from "react-query/devtools";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient());
   return (
-    <SessionProvider session={session}>
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <Component {...pageProps} />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </Hydrate>
-      </QueryClientProvider>
-    </SessionProvider>
+    <>
+      <NextNProgress />
+      <SessionProvider session={session}>
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            <Component {...pageProps} />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </Hydrate>
+        </QueryClientProvider>
+      </SessionProvider>
+    </>
   );
 }
 
